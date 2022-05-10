465efbac36dab423de46899d556c19cf378f8439
Albin Zeqiri <albin.zeq@gmail.com>
2022-05-10 16:57:04 +0200

    Update

--- dataloaders/path.py
+++ dataloaders/path.py
@@ -2,7 +2,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(database):
         if database == 'tcs':
-            return '~/ma/dataset/logs/'
+            return '/mnt/hdd/albin_zeqiri/ma/dataset/logs/'
             #return "D:/csv-merger/out/"
         else:
             print('Database {} not available.'.format(database))
