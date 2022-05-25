b8507234fd6da4eb3f3940e32295d4f537c066ac
Albin Zeqiri <albin.zeq@gmail.com>
2022-05-25 18:16:19 +0200

    lstm adjustment

--- network/learning_models.py
+++ network/learning_models.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 #from network.computations import categoryFromOutput
 
 class MLP(nn.Module):
@@ -25,13 +25,16 @@ class MLP(nn.Module):
         return torch.squeeze(x)
 
 class RNN(nn.Module):
-    def __init__(self, in_features, num_classes, n_layers=1, hidden_dim=128, dropout=0.2):
+    def __init__(self, in_features, num_classes, n_layers=1, hidden_dim=256, dropout=0.2):
         super(RNN, self).__init__()
         """
         LSTM classifier without activation layer
         """
-        
+        self.n_layers = n_layers
         self.lstm = nn.LSTM(in_features, hidden_dim, n_layers, batch_first=True)
+        self.dp_layer = nn.Dropout(dropout)
+        if n_layers > 1:
+            self.lstm = self.lstm = nn.LSTM(in_features, hidden_dim, n_layers, batch_first=True, dropout=dropout)
         self.fc = nn.Linear(hidden_dim, num_classes)
         self.activation = nn.Softmax(dim=1)
         
@@ -40,7 +43,8 @@ class RNN(nn.Module):
         self.lstm.flatten_parameters() #use multi GPU capabilities
         _, (h_t, _) = self.lstm(x)
         x = h_t[-1]
-        #x = self.dp(x)
+        if self.n_layers > 1:
+            x = self.dp_layer(x)
         x = self.fc(x)
         x = self.activation(x)
         #x *= 3 #scale to [-3,3]
@@ -71,7 +75,7 @@ class RandomForest():
     def __init__(self, n_estimators=None, max_depth=None, critirion='gini', bootstrap=True, cv=True, max_features="log2"):
         self.rf = RandomForestClassifier()
         if not cv:
-            self.rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, random_state=0, criterion=critirion, bootstrap=bootstrap, verbose=2, min_samples_leaf=6)
+            self.rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, criterion=critirion, bootstrap=bootstrap, verbose=2, class_weight=self.weight_dict())
         
     
     def fit(self, train_inputs, train_labels):
@@ -84,6 +88,35 @@ class RandomForest():
     def predict(self, x):
         return self.rf.predict(x)
     
+    def weight_func(self, x):
+        return 0.1*x^2+1
+    
+    def weight_dict(self):
+        weights = {-3: self.weight_func(-3),
+                   -2: self.weight_func(-2),
+                   -1: self.weight_func(-1),
+                   0: self.weight_func(0),
+                   1: self.weight_func(1),
+                   2: self.weight_func(2),
+                   3: self.weight_func(3)}
+        return weights
+
+class RandomForestRegressor():
+    def __init__(self, n_estimators=None, max_depth=None, criterion='squared_error', bootstrap=True, cv=True, max_features="log2"):
+        self.rf = RandomForestClassifier()
+        if not cv:
+            self.rf = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, max_features=max_features, bootstrap=bootstrap)
+        
+    
+    def fit(self, train_inputs, train_labels):
+        print("Fitting random forest classifier to data..")
+        self.rf.fit(train_inputs, train_labels)
+    
+    def feature_importances(self):
+        return self.rf.feature_importances_
+        
+    def predict(self, x):
+        return self.rf.predict(x)
 
 if __name__ == "__main__":
     t = torch.randn((4,10,4))
