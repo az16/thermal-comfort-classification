ec419dd651a23b578b6e8d7e73d109bc2978408d
Albin Zeqiri <albin.zeq@gmail.com>
2022-06-02 16:07:05 +0200

    Fix paths

--- dataloaders/tc_dataloader.py
+++ dataloaders/tc_dataloader.py
@@ -98,10 +98,10 @@ class TC_Dataloader(BaseDataset):
         #find files
         print("Searching for {0} files..".format(self.split))
         file_names =  [] #os.listdir(Path.db_root_dir("tcs"))
-        #with open("./dataloaders/splits/{0}_{1}.txt".format(self.split, 60)) as file:
-        #     lines = file.readlines()
-        #     file_names = [line.rstrip() for line in lines]
-        # assert len(file_names) > 0; "No files found at {0}".format(Path.db_root_dir("tcs"))
+        with open("./dataloaders/splits/{0}_{1}.txt".format(self.split, 60)) as file:
+            lines = file.readlines()
+            file_names = [line.rstrip() for line in lines]
+        assert len(file_names) > 0; "No files found at {0}".format(Path.db_root_dir("tcs"))
             
         file_names = [Path.db_root_dir("tcs")+x for x in file_names]
         print("Found {0} {1} files at {2}".format(len(file_names),self.split,Path.db_root_dir("tcs")))
