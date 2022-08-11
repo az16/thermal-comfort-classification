a6c45017afbc8d9a7c6ee4948a941bab1ff12d4f
sebastian <sebastian.hartwig@uni-ulm.de>
2022-08-11 13:38:11 +0200

    fix dev and name arguments

--- train.py
+++ train.py
@@ -58,6 +58,8 @@ if __name__ == "__main__":
 
     callbacks = []
 
+    if args.dev: args.name = None
+
     if args.name:
         callbacks += [pl.callbacks.lr_monitor.LearningRateMonitor()]
     
