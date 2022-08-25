fix accuracy computation

metrics.py
@@ -228,19 +228,10 @@ def mae(preds, targets):
 class Accuracy(nn.Module):
     def __init__(self):
         super().__init__()
-        self.current_correct = 0
-        self.current_total = 0
-    
-    def reset(self):
-        self.current_correct = 0
-        self.current_total = 0
-    
     def forward(self, y_hat, y):
-        # ones = torch.sum(torch.all((torch.round(y_hat) == 1), dim=1))
-        # tmp = torch.zeros_like(y_hat)
-        self.current_correct += torch.sum(torch.all((torch.round(y_hat) == y), dim=1)).item()
-        self.current_total += y_hat.shape[0]
-        return self.current_correct/self.current_total
+        current_correct = torch.sum(torch.all((torch.round(y_hat) == y), dim=1)).item()
+        current_total = y_hat.shape[0]
+        return current_correct/current_total
     
 
 # METRICS = plf.__dict__ #pl.metrics.functional.__dict__ 