e4147782b1c3d6f06f501610cff83fccc0fe0246
Albin Zeqiri <albin.zeq@gmail.com>
2022-09-07 13:36:00 +0200

    update pmv code

--- dataloaders/pmv_loader.py
+++ dataloaders/pmv_loader.py
@@ -39,7 +39,7 @@ class PMV_Results():
         print("Adding to dataframe..")
         
         #contiuous preds
-        self.df["PMV"] = pmv(self.df["Radiation-Temp"], self.df["Clothing-Level"], self.df["Ambient_Temperature"], self.df["Ambient_Humidity"]) #calls pythermalcomfort api to calculate PMV based on ASHRAE standard
+        self.df["PMV"] = pmv_calc(self.df["Radiation-Temp"], self.df["Clothing-Level"], self.df["Ambient_Temperature"], self.df["Ambient_Humidity"]) #calls pythermalcomfort api to calculate PMV based on ASHRAE standard
         #round preds to compare for accuracy 
         self.df["PMV_rounded"] = np.round(self.df["PMV"], 0).astype(np.int64)
         
@@ -48,38 +48,39 @@ class PMV_Results():
         #clf_tree(self.df["PMV_rounded"].values, self.df["Label"].values, [-3, -2, -1, 0, 1, 2, 3], "PMV_cm") #Use this to generate a confusion matrix
         print("Computing accuracy..")
         correct_7 = np.sum(self.df["PMV_rounded"].values==self.df["Label"].values)
-        correct_3 = np.sum(reduce_scale(self.df, scale=3)==narrow_labels(self.df, scale=3)["Label"].values)
-        correct_2 = np.sum(reduce_scale(self.df, scale=2)==narrow_labels(self.df, scale=2)["Label"].values)
+        correct_3 = np.sum(reduce_scale(self.df, "PMV_rounded", scale=3)==reduce_scale(self.df, "Label", scale=3))
+        correct_2 = np.sum(reduce_scale(self.df, "PMV_rounded", scale=2)==reduce_scale(self.df, "Label", scale=2))
         total = self.df.shape[0]
 
         self.pmv_accuracy_7 = correct_7/total
         self.pmv_accuracy_3 = correct_3/total
         self.pmv_accuracy_2 = correct_2/total
 
-def reduce_scale(df, scale=2):
+def reduce_scale(df, column, scale=2):
     if scale==2:
         #print("2-Point scale transformation.")
-        #df.loc[(df["PMV_rounded"] == 0), "PMV_rounded"] = 0
-        df.loc[(df["PMV_rounded"] == 1), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == -1), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == -3), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == -2), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == 2), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == 3), "PMV_rounded"] = 1
-       
-        return df["PMV_rounded"].values
+        #df.loc[(df[column] == 0), column] = 0
+        #df.loc[(df[column] == 1), column] = 1
+        df.loc[(df[column] == -1), column] = 1
+        df.loc[(df[column] == -3), column] = 1
+        df.loc[(df[column] == -2), column] = 1
+        df.loc[(df[column] == 2), column] = 1
+        df.loc[(df[column] == 3), column] = 1
+        print("{0}: all 1 {1}, all 0 {2}".format(column, (df[column].values==1).all(), (df[column].values==0).all()))
+        return df[column].values
     elif scale==3:
         #print("3-Point scale transformation.")
 
-        df.loc[(df["PMV_rounded"] == -1), "PMV_rounded"] = 0
-        df.loc[(df["PMV_rounded"] == -3), "PMV_rounded"] = -1
-        df.loc[(df["PMV_rounded"] == -2), "PMV_rounded"] = -1
-        df.loc[(df["PMV_rounded"] == 0), "PMV_rounded"] = 0
-        df.loc[(df["PMV_rounded"] == 1), "PMV_rounded"] = 0
-        df.loc[(df["PMV_rounded"] == 2), "PMV_rounded"] = 1
-        df.loc[(df["PMV_rounded"] == 3), "PMV_rounded"] = 1
-        return df["PMV_rounded"].values
-    return df["PMV_rounded"].values
+        df.loc[(df[column] == -1), column] = 0
+        df.loc[(df[column] == -3),column] = -1
+        df.loc[(df[column] == -2), column] = -1
+        #df.loc[(df[column] == 0), column] = 0
+        df.loc[(df[column] == 1), column] = 0
+        df.loc[(df[column] == 2), column] = 1
+        df.loc[(df[column] == 3), column] = 1
+        print("{0}: all 1 {1}, all 0 {2}".format(column, (df[column].values==1).all(), (df[column].values==0).all()))
+        return df[column].values
+    return df[column].values
    
 def clf_tree(x,y,label_names, name):
     """
